fcd26290a0a000e8d8afa72c9e8424b2e1734c9b
bh206103bh <123520200+bh206103bh@users.noreply.github.com>
2023-01-31 15:01:27 +1000

    Update count_letters.py

--- Unit2_Create_With_Code/count_letters.py
+++ Unit2_Create_With_Code/count_letters.py
@@ -3,6 +3,7 @@ answer = input("Do you want me to count the characters of a book title for you? 
 if answer =="yes":
   book = input("What is the title of the book?")
 length = str(len(book))
-print("There are " ,length, " symbols in the title " ,book, " including the spaces.")
+#print("There are " ,length, " symbols in the title " ,book, " including the spaces.")
+print(f"There are {len(book)} characters in the book title {book} including spaces.
 else:
   input("Ok")
